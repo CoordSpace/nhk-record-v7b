d9763cc7850555b4247c1a1ed2a083e1cd245601
chrominance <luminance@chrominance.net>
2024-07-21 22:27:59 -0400

    Add test for ffmpeg command with thumbnail

--- src/__tests__/ffmpeg.test.ts
+++ src/__tests__/ffmpeg.test.ts
@@ -2,40 +2,27 @@ import { expect, jest, test } from '@jest/globals';
 import { execute } from '../utils';
 import { postProcessRecording } from '../ffmpeg';
 
+const inputPathNoThumbnail = 'inputPath.mp4';
+const inputPathWithThumbnail = 'inputPathWithThumbnail.mp4';
+
 jest.mock('../utils', () => {
     const stdout: Array<string> = [];
     const stderr: Array<string> = [];
     const mockExecute = jest.fn<typeof execute>(async (command, args) => {
         if (command == 'ffprobe') {
-            const mockFfprobeObject = [
-                '{',
-                '   "format": {',
-                '       "filename": "Rockie and Her Friends - 4034 - 001.mp4",',
-                '       "nb_streams": 2,',
-                '       "nb_programs": 0,',
-                '       "format_name": "mov,mp4,m4a,3gp,3g2,mj2",',
-                '       "format_long_name": "QuickTime / MOV",',
-                '       "start_time": "0.017000",',
-                '       "duration": "618.709333",',
-                '       "size": "383262174",',
-                '       "bit_rate": "4955634",',
-                '       "probe_score": 100,',
-                '       "tags": {',
-                '           "major_brand": "isom",',
-                '           "minor_version": "512",',
-                '           "compatible_brands": "isomiso2avc1mp41",',
-                '           "date": "2024-04-06T03:40:00.000Z",',
-                '           "encoder": "Lavf58.50.100",',
-                '           "description": "Puppet action show for children. In a distant post-human future where surviving creatures have formed a community, new-breed dinosaur girl Rockie sets out on hilarious adventures with her schoolmates.",',
-                '           "show": "Rockie and Her Friends",',
-                '           "episode_id": "001",',
-                '           "network": "NHK World"',
-                '       }',
-                '   }',
-                '}'
-            ];
+            // getStreamCount
+            let nbStreams = 2;
+            if (args.includes(inputPathWithThumbnail)) {
+                nbStreams = 3;
+            }
+            const mockFfprobeObject = [JSON.stringify({
+                format: {
+                    nb_streams: nbStreams
+                }
+            })];
             return Promise.resolve({ stdout: mockFfprobeObject, stderr: [] });
         } else {
+            // all ffmpeg calls
             return Promise.resolve({ stdout: stdout, stderr: stderr });
         }
     });
@@ -49,10 +36,14 @@ jest.mock('../utils', () => {
 
 jest.mock('../logger');
 
+beforeEach(() => {
+    jest.mocked(execute).mockClear();
+});
+
 describe('ffmpeg', () => {
     describe('postProcessRecording', () => {
         it('should run ffmpeg once with the right arguments when no crop and no thumbnail is given', async () => {
-            const inputPath = 'inputPath.mp4';
+            const inputPath = inputPathNoThumbnail;
             const outputPath = 'outputPath.mp4';
             const startMs = 124687;
             const endMs = 367110;
@@ -64,11 +55,41 @@ describe('ffmpeg', () => {
                 [
                     '-y',
                     '-i', inputPath,
-                    '-ss', '' + startMs / 1000,
-                    '-to', '' + endMs / 1000,
+                    '-ss', `${startMs / 1000}`,
+                    '-to', `${endMs / 1000}`,
                     '-codec', 'copy',
                     '-map', '0:0',
                     '-map', '0:1',
+                    '-map_metadata', '0',
+                    '-f', 'mp4',
+                    outputPath
+                ]
+            );
+        });
+
+        it('should run ffmpeg once with the right arguments when no crop and embedded thumbnail is given', async () => {
+            const inputPath = inputPathWithThumbnail;
+            const outputPath = 'outputPath.mp4';
+            const startMs = 124687;
+            const endMs = 367110;
+            await postProcessRecording(inputPath, outputPath, startMs, endMs, []);
+
+            expect(execute).toHaveBeenCalledTimes(2);
+            expect(execute).toHaveBeenCalledWith(
+                'ffmpeg',
+                [
+                    '-y',
+                    '-i', inputPath,
+                    '-i', inputPath,
+                    '-ss', `${startMs / 1000}`,
+                    '-to', `${endMs / 1000}`,
+                    '-codec', 'copy',
+                    '-filter_complex', `[1:2]setpts=PTS+${startMs / 1000}/TB[tn]`,
+                    '-map', '0:0',
+                    '-map', '0:1',
+                    '-map', '[tn]',
+                    '-codec:v:1', 'mjpeg',
+                    '-disposition:v:1', 'attached_pic',
                     '-map_metadata', '0',
                     '-f', 'mp4',
                     outputPath
